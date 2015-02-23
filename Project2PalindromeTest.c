b6c4dd636b6e3093723ab4768e1a65184d84aad2
Davis Foster <davis@antumbra.io>
2015-02-23 01:41:28 -0500

    Fixed all bugs.  Appears to work correctly provided input is within bounds.

--- Project2PalindromeTest.c
+++ Project2PalindromeTest.c
@@ -29,7 +29,7 @@ Purpose: Takes input, tests if it's a palindrome or not.
 Returns:  Palindrome T/F and failure data.
 ---------------------------------------------------------------------------*/
 
-int digits;
+/*int digits;
 int numFor[5];
 int numBak[5];
 int truth = 1; //Lazy Bool
@@ -37,26 +37,41 @@ char charFor[5];
 char charBak[5];
 char charIn[5];
 char sel; 
+int ch;*/
 
 int main()
 {
     while(1){
-   
+   int digits;
+int numFor[5];
+int numBak[5];
+int truth = 1; //Lazy Bool
+char charFor[5];
+char charBak[5];
+char charIn[5];
+char sel; 
+int ch;
     printf("\nEnter n for number, w for word, or t to terminate: ");
    
     scanf("%c[^\n]", &sel);
-    
+    //printf("\nSel is: %c", &sel);
+   
     if (sel == 'n'){
+        printf("\n");
         printf("\nEnter a 5-digit integer: ");
         scanf( "%i", &digits);
+        //printf("\nInput: %i",&digits);
         //Yes, I'm seeing how poorly I can do this.  Efficiency?  Pfft.
         for(int i = 0; i<5; i++){  //Overflow?  LOL.
+
             numFor[i] = digits%10;
             digits = digits / 10;
         }
+
        
         for(int i = 0; i<5; i++){
             numBak[i] = numFor[(4-i)];
+            //printf("\nNum Is: %d", numBak[i]);
         }
         
         for(int i = 0; i<5; i++){
@@ -71,10 +86,12 @@ int main()
         else{
             printf("\nIt is not a palindrome\n");
         }
+        while ((ch =getchar()) != EOF && ch != '\n');
 
 
     }
     if (sel == 'w'){
+        printf("\n");
         printf("\nEnter a 5-letter word: ");
         scanf( "%s", &charIn);
         for(int i = 0; charIn[i] != '\0'; i++){  //gotta love C strings
@@ -83,10 +100,12 @@ int main()
         
         for(int i = 0; i<5; i++){
             charBak[i] = charFor[(4-i)];
+            //printf("\nNum Is: %c", charBak[i]);
         }
         
         for(int i = 0; i<5; i++){
             if (charFor[i] != charBak[i]){
+                
                 truth = 0;
             }
         }
@@ -97,17 +116,28 @@ int main()
         else{
             printf("\nIt is not a palindrome\n");
         }
+        while ((ch =getchar()) != EOF && ch != '\n');
     }
     if (sel == 't'){
+        printf("\n");
         printf("\n***** Program Terminated *****\n");
+        printf("\n");
         exit(0);
     }
     if (sel != 'w' && sel != 'n' && sel != 't'){
+        printf("\n");
         printf("\nWrong input, please try again\n");
-        sel = 't';
+        while ((ch =getchar()) != EOF && ch != '\n');
     }
+    
 
     
     }
     return (EXIT_SUCCESS);
 }
+
+//void clearBuffer()
+//{
+//    int ch;
+  //  while ((ch =getchar()) != EOF && ch != '\n');
+//}
