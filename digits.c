b7854f59c514982c91d71faa1f1b710391ddf40c
Davis Foster <davis@antumbra.io>
2015-03-30 14:40:26 -0400

    Almost working, missing the divisible checker functionality.

--- digits.c
+++ digits.c
@@ -17,7 +17,9 @@ Include Section
 #include<stdbool.h>
 
 void separate(long int input);
-void print(int ayy[], int size);
+void print(int ayy[], int size, long int input);
+int digits_different(int occurances[], int size);
+int divisible(int ayy[], int size, long int input);
 
 
 
@@ -72,29 +74,79 @@ void separate(long int input)
     }
 
 
-    print(sep, size);
+    print(sep, size, input);
 
 }
 
-void print(int ayy[], int size)
+void print(int ayy[], int size, long int input)
 {
+    //int val;
     int occurances[10] = {0,0,0,0,0,0,0,0,0,0}; //LOL
-    for(int x = 0; x<10; x++){ 
-        
-        if(ayy[x] == x){
-            occurances[x] = ++occurances[x];
-        }
-        
+    for(int x = 0; x<size; x++){ 
+        //val = ayy[x];
+        occurances[ayy[x]] = occurances[ayy[x]]+1;
     }
 
     printf("\n");
     printf("\nDigits:         0  1  2  3  4  5  6  7  8  9\n");
     printf("\nOccurrences: ");
     printf("   ");
-    for(int i = 0; i<size; i++){ 
+    for(int i = 0; i<10; i++){ 
        printf("%d  ",occurances[i]);
     }
+    printf("\n");
+    int chek = 0;
+    for(int y = 0; y<10; y++){
+        if((occurances[y] > 1)){
+            printf("\nWrong input for the second part.\n");
+            printf("\nInput should not contain each digit more than once.\n");
+            chek = 1;
+            break;
+        } 
+    }
+    if((occurances[0] > 0) && (chek != 1)){
+            printf("\nWrong input for the second part.\n");
+            printf("\nInput should not contain zero.\n");
+    }
+    if((occurances[0] > 0) && (chek == 1)){
+            printf("\nInput should not contain zero.\n");
+    }
+    chek = 0;
+/*
+    if((divisible(ayy, size, input) == 1) && (chek == 0)){
+                printf("\nIs divisible\n");
+            
+    }
+    */
+//int valid = digits_different(occurances[], size);
 
+
+}
+
+int digits_different(int occurances[], int size)
+{
+    int ret = 1;
+    for(int i = 0; i<10; i++){ 
+       if (occurances[i] > 1)
+       {
+           ret = 0;
+           break;
+       }
+    }
+    return ret;
+
+}
+
+int divisible(int ayy[], int size, long int input){
+    for(int i = 0; i<size; i++){ 
+       if (ayy[i] == 0){
+           continue;
+       }
+       else if(input % i != 0){
+            return 0;
+       }
+    }
+    return 1;
 
 }
 
