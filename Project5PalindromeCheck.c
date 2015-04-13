258dd81ccae984b4ee69c50bdd5550e703c33870
Davis Foster <davis@antumbra.io>
2015-04-13 03:11:40 -0400

    Fully function.  Output needs to be formatted before submission.

--- Project5PalindromeCheck.c
+++ Project5PalindromeCheck.c
@@ -33,7 +33,7 @@ Returns:  Palindrome T/F and failure data.
 
 int main()
 {
-    while(1){
+    while(1){ //So I heard you're a fan of nested loops...
         char input[81];
         int x;
         int ayyLen = 0;
@@ -47,7 +47,7 @@ int main()
                     {
                         input[x]=input[x+1]; //gets rid of character by setting space equal to next position in array
                     }
-                    input[x]='\0';
+                    input[x]='\0'; //null term
                 }
             }
         }
@@ -55,10 +55,10 @@ int main()
         for (int z = 0; input[z] !='\0'; ++z)
         {
             input[z]=tolower(input[z]); //ensures that all characters are lower case
-            ayyLen++;
+            ayyLen++; //counts values in array
         }
-        puts(input);
-        printf("\n%d\n", ayyLen);
+        //puts(input);
+        //printf("\n%d\n", ayyLen);
 
         //char forward[ayyLen];
         char back[ayyLen+1];
@@ -69,18 +69,34 @@ int main()
         }
         */
         int q;
-        for(q = 0; q<ayyLen; ++q)
+        for(q = 0; q<ayyLen; ++q) //I'm aware of how god awful this is.
         {
             back[q] = input[(ayyLen-q-1)];
             //printf("%c\n",input[q]);  
             //printf("%c\n",back[q]);  
         }
-        back[q]='\0';
+        back[q]='\0'; //null term
         //puts(input);
         //puts(forward);
-        puts(back);
+        //puts(back);
         //printf("\n%d\n", ayyLen);
-    
+        bool truth = true;
+        for(int i = 0; i<ayyLen; i++)
+        {
+            if (input[i] != back[i]){
+                truth = false;
+            }
+        }
+
+        if (truth == false)
+        {
+            printf("Not Palindrome.\n");
+        }
+
+        if (truth == true)
+        {
+            printf("Palindrome!\n");
+        }
 
         
     }
