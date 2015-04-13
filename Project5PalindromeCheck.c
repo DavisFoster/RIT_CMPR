dcd008dc2f64d2a1478c58ba3f42fd0f8dfff00c
Davis Foster <davis@antumbra.io>
2015-04-13 03:52:52 -0400

    Completed.  Appears to function correctly, IO is properly formatted and everything seems stable.

--- Project5PalindromeCheck.c
+++ Project5PalindromeCheck.c
@@ -22,26 +22,29 @@ Include Section
 #include<stdlib.h>
 #include<limits.h>
 #include<stdbool.h>
-#include<time.h>
 #include<ctype.h>
+#include<string.h>
 
 /*--------------------------- main function -------------------------------
 Purpose: Takes input, tests if it's a palindrome or not.
 
-Returns:  Palindrome T/F and failure data.
+Returns:  Palindrome T/F and exit.  
+
+Yes.  I did it all in one fuction.  Bad practice?  Maybe.
 ---------------------------------------------------------------------------*/
 
 int main()
 {
+    //printf("\n");
     while(1){ //So I heard you're a fan of nested loops...
         char input[81];
         int x;
         int ayyLen = 0;
-        printf("\nEnter Yo Shit\n");
+        printf("\nEnter a string: ");
         if (fgets(input, 81, stdin) != NULL){ //Takes input
             for(int i=0; input[i]!='\0'; ++i) //Iterates through all of input
             {
-                while (! ( (input[i]>='a'&&input[i]<='z') || (input[i]>='A'&&input[i]<='Z' || input[i]=='\0') ) ) //Runs while input is not within bounds
+                while (! ( (input[i]>='a'&&input[i]<='z') || ((input[i]>='A'&&input[i]<='Z') || input[i]=='\0') ) ) //Runs while input is not within bounds
                 {   
                     for(x=i;input[x]!='\0';++x)
                     {
@@ -51,51 +54,46 @@ int main()
                 }
             }
         }
-        //puts(input);
+        
         for (int z = 0; input[z] !='\0'; ++z)
         {
             input[z]=tolower(input[z]); //ensures that all characters are lower case
             ayyLen++; //counts values in array
         }
-        //puts(input);
-        //printf("\n%d\n", ayyLen);
+        
+        char back[ayyLen+1]; //mirrored input.  Needs +1 to add null term.
 
-        //char forward[ayyLen];
-        char back[ayyLen+1];
-
-        /*for(int i = 0; i<ayyLen; ++i)
-        {
-            forward[i] = input[i];    
-        }
-        */
-        int q;
+        int q; //iterator needs to exist outside of loop
         for(q = 0; q<ayyLen; ++q) //I'm aware of how god awful this is.
         {
-            back[q] = input[(ayyLen-q-1)];
-            //printf("%c\n",input[q]);  
-            //printf("%c\n",back[q]);  
+            back[q] = input[(ayyLen-q-1)];  
         }
         back[q]='\0'; //null term
-        //puts(input);
-        //puts(forward);
-        //puts(back);
-        //printf("\n%d\n", ayyLen);
+        
+        //This section checks for palindromal text. VV
+        char done[5] = {'d', 'o', 'n', 'e', '\0'};
+        if(strcmp(done,input) == 0){
+            printf("\n***** Program Terminated *****\n");
+            return (EXIT_SUCCESS);
+        }
+
         bool truth = true;
         for(int i = 0; i<ayyLen; i++)
         {
-            if (input[i] != back[i]){
+            if (input[i] != back[i])
+            {
                 truth = false;
             }
         }
 
         if (truth == false)
         {
-            printf("Not Palindrome.\n");
+            printf("\nIt is not a palindrome\n");
         }
 
         if (truth == true)
         {
-            printf("Palindrome!\n");
+            printf("\nIt is a palindrome\n");
         }
 
         
